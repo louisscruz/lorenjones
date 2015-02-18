43d58ffcdba212e7f15aca3b4f2de445cef8cfce
louisscruz <louisstephancruz@me.com>
2015-02-17 23:23:00 -0500

    Attempt to fix date format and css

--- client/app/events/events.controller.js
+++ client/app/events/events.controller.js
@@ -4,6 +4,9 @@ angular.module('louiscruzApp')
   .controller('EventsCtrl', function ($scope, $http, socket) {
     $scope.events = [];
     $scope.options = {scrollwheel: false};
+    $scope.dateOptions = {
+      format: 'dddd, mmmm dddd',
+    };
 
     $http.get('/api/events').success(function(events) {
       $scope.events = events;
