9932f098575d67abb95c1cbb040557e403e0f584
louisscruz <louisstephancruz@me.com>
2015-02-19 14:04:22 -0500

    seed db

--- server/config/seed.js
+++ server/config/seed.js
@@ -41,7 +41,7 @@ User.find({}).remove(function() {
     provider: 'local',
     role: 'admin',
     name: 'Admin',
-    email: 'admin@admin.com',
+    email: 'lsc@juilliard.edu',
     password: 'admin'
   }, function() {
       console.log('finished populating users');
