f24c7c96e760d6fba6faf6bc6cc5cbb6adcf1c0d
louisscruz <louisstephancruz@me.com>
2015-02-26 21:27:59 -0500

    Fix max-height content, scale back plangular, and add some styling to events with info

--- client/app/works/works.controller.js
+++ client/app/works/works.controller.js
@@ -3,7 +3,6 @@
 angular.module('louiscruzApp')
   .controller('WorksCtrl', function ($scope, $http, socket) {
     $scope.works = [];
-    $scope.theBestVideo = 'sMKoNBRZM1M';
 
     $http.get('/api/works').success(function(works) {
       $scope.works = works;
