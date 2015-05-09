9878b6626cecfb28146dc5462ead3a7e3dbbcc36
louisscruz <louisstephancruz@me.com>
2015-05-09 00:50:29 -0400

    Add basic instagram factory

--- client/app/about/about.controller.js
+++ client/app/about/about.controller.js
@@ -1,11 +1,30 @@
 'use strict';
 
 angular.module('louiscruzApp')
-  .controller('AboutCtrl', function ($scope, $http, socket) {
+  .controller('AboutCtrl', function ($scope, $http, socket, $interval, instagram) {
     $scope.bioEntries = [];
 
     $http.get('/api/bio_entries').success(function(bioEntries) {
       $scope.bioEntries = bioEntries;
       socket.syncUpdates('bio_entry', $scope.bioEntries);
     });
+
+    $scope.pics = [];
+    $scope.have = [];
+    $scope.orderBy = "-likes.count";
+    $scope.getMore = function() {
+      instagram.fetchPopular(function(data) {
+          for(var i=0; i<data.length; i++) {
+            if (typeof $scope.have[data[i].id]==="undefined") {
+              $scope.pics.push(data[i]) ;
+              $scope.have[data[i].id] = "1";
+            }
+          }
+      });
+    };
+    $scope.getMore();
+
+    $scope.tags = [
+        'Bootstrap', 'AngularJS', 'Instagram', 'Factory'
+    ]
   });
--- client/app/factories/instaFact/instaFact.service.js
+++ client/app/factories/instaFact/instaFact.service.js
@@ -1,16 +1,17 @@
 'use strict';
 
 angular.module('louiscruzApp')
-  .factory('instaFact', function () {
-    // Service logic
-    // ...
+  .factory('instagram', ['$http',
+      function($http) {
+          return {
+              fetchPopular: function(callback) {
 
-    var meaningOfLife = 42;
+                  var endPoint = "https://api.instagram.com/v1/media/popular?client_id=642176ece1e7445e99244cec26f4de1f&callback=JSON_CALLBACK";
 
-    // Public API here
-    return {
-      someMethod: function () {
-        return meaningOfLife;
+                  $http.jsonp(endPoint).success(function(response) {
+                      callback(response.data);
+                  });
+              }
+          }
       }
-    };
-  });
+  ])
