d1ac6f11eb5f92cab2c295b09a12518ad7f69f1a
louisscruz <louisstephancruz@me.com>
2015-05-13 11:13:46 -0400

    Add jpg to htaccess expires

--- client/app/factories/instaFact/instaFact.service.js
+++ client/app/factories/instaFact/instaFact.service.js
@@ -2,16 +2,14 @@
 
 angular.module('louiscruzApp')
   .factory('instagram', ['$http',
-      function($http) {
-          return {
-              fetchPopular: function(callback) {
-
-                  var endPoint = "https://api.instagram.com/v1/users/1545705357/media/recent?callback=JSON_CALLBACK&client_id=65568cd5ceaf443a8816303a0fa6de61";
-
-                  $http.jsonp(endPoint).success(function(response) {
-                      callback(response.data);
-                  });
-              }
-          }
+    function($http) {
+      return {
+        fetchPopular: function(callback) {
+          var endPoint = "https://api.instagram.com/v1/users/1545705357/media/recent?callback=JSON_CALLBACK&client_id=65568cd5ceaf443a8816303a0fa6de61";
+          $http.jsonp(endPoint).success(function(response) {
+          callback(response.data);
+          });
+        }
       }
+    }
   ])
