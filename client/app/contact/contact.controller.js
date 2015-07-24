77bcb1c6ab9560a894c411c54ae2cbde8912cdf5
louisscruz <louisstephancruz@me.com>
2015-07-23 22:39:04 -0400

    Add basic upgraded form validation (email)

--- client/app/contact/contact.controller.js
+++ client/app/contact/contact.controller.js
@@ -3,20 +3,26 @@
 angular.module('lorenjonesApp')
   .controller('ContactCtrl', function ($scope, $http, alertFact, $location) {
     $scope.contact = {};
+    $scope.sending = false;
+    $scope.cache = null;
     $scope.submitForm = function(isValid) {
       if (isValid) {
+        $scope.sending = true;
         var data = ({
           name: $scope.contact.name,
           email: $scope.contact.email,
           message: $scope.contact.message
         });
+        $scope.cache = data;
         $http.post('/api/contact', data)
           .success(function(d) {
             alertFact.add('success', d.name + ', thank you for the message. I\'ll get back to you soon!');
+            $scope.sending = false;
             $location.path('/');
           })
           .error(function(d) {
-            alertFact.add('danger', 'We could not deliver your message.');
+            $scope.sending = false;
+            alertFact.add('danger', 'We could not deliver your message. Modify your message and try again, or send your message to louisstephancruz@me.com.');
           })
       }
     }
