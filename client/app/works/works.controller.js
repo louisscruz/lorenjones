77edc6d9484bac61b4a6ff5e16bf22e3855c32eb
louisscruz <louisstephancruz@me.com>
2015-07-27 12:03:05 -0400

    Fix works toggle

--- client/app/works/works.controller.js
+++ client/app/works/works.controller.js
@@ -4,7 +4,7 @@ angular.module('lorenjonesApp')
   .controller('WorksCtrl', function ($scope) {
     $scope.info = '';
     $scope.toggle = function(id) {
-      if (id !== '') {
+      if (id === '' || id !== $scope.info) {
         $scope.info = id;
       } else {
         $scope.info = '';
