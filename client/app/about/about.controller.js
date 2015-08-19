08929d8845a6bcfb290e7a697ec6b40ca76e3adb
louisscruz <louisstephancruz@me.com>
2015-08-18 23:02:48 -0400

    Add basic flickr rewrite

--- client/app/about/about.controller.js
+++ client/app/about/about.controller.js
@@ -3,25 +3,30 @@
 angular.module('lorenjonesApp')
   .controller('AboutCtrl', function ($scope, $http, socket, $interval, flickr, Lightbox) {
     $scope.bioEntries = [];
-
     $http.get('/api/bio_entries', {cache: true}).success(function(bioEntries) {
       $scope.bioEntries = bioEntries;
       socket.syncUpdates('bio_entry', $scope.bioEntries);
     });
-
     $scope.pics = [];
-    $scope.src = 'photos_public.gne?id=134139109@N08';
-    $scope.loadPhotos = function() {
-      flickr.query($scope.src)
+    var per_page = 12;
+    $scope.page = 1;
+    var src = 'flickr.photosets.getPhotos';
+    $scope.loadPhotos = function(page) {
+      flickr.query(src, per_page, page)
       .then(function(data) {
-        for(var i=0; i<data.length; i++) {
+        for (var i = 0, len = data.length; i < len; i++) {
           $scope.pics.push(data[i]);
         }
       });
     };
-    $scope.loadPhotos();
-    $scope.openLightboxModal = function (index) {
-      Lightbox.openModal($scope.pics, index);
+    $scope.loadPhotos($scope.page);
+    $scope.loadMore = function() {
+      $scope.page++;
+      console.log($scope.page);
+      $scope.loadPhotos($scope.page);
+      console.log($scope.pics);
     };
-
+    $scope.openLightboxModal = function(index) {
+      Lightbox.openModal($scope.largePics, index);
+    };
   });
--- client/app/factories/flickrFact/flickrFact.service.js
+++ client/app/factories/flickrFact/flickrFact.service.js
@@ -3,37 +3,32 @@ angular.module('lorenjonesApp')
   .factory('flickr', ['$http', '$q',
     function($http, $q) {
       var o = {};
-      o.query = function(src) {
+      var api_key = 'b288d1360b00e2f8584150f7da3ff3ef';
+      var user_id = 'id=134139109@N08';
+      var photoset_id = '72157657015377619';
+      o.query = function(src,per_page, page) {
         var deferred = $q.defer();
         if (o.photos) {
           deferred.resolve(o.photos);
         } else {
-          $http.jsonp('http://www.flickr.com/services/feeds/' + src + '&lang=en-us&format=json&jsoncallback=JSON_CALLBACK', {
-            cache: true
+          $http.jsonp('https://api.flickr.com/services/rest/?&method=flickr.photosets.getPhotos&api_key=b288d1360b00e2f8584150f7da3ff3ef&photoset_id=72157657015377619&user_id=134139109@N08&per_page=' + per_page + '&page=' + page + '&format=json&jsoncallback=JSON_CALLBACK', {
+            cache: false
           })
-          .success(function(result) {
-            o.photos = result.items;
-
-            angular.forEach(o.photos, function(p) {
-              p.url = p.media.m.replace('_m', '_b');
-              p.caption = p.title;
-              p.thumbnail = p.media.m;
-            });
+          .then(function(result) {
+            //if has photos, concat photos to old photos, else:
+            o.photos = result.data.photoset.photo;
+            for (var i = 0, len = o.photos.length; i < len; i++) {
+              var photo = o.photos[i];
+              o.photos[i].thumbnail = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '_n.jpg';
+              o.photos[i].url = 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg';
+            }
             deferred.resolve(o.photos);
-          })
-          .error(function() {
+          }, function(error) {
             deferred.reject('Could not get json');
           });
         }
         return deferred.promise;
       };
-      //o.largeQuery = function(src) {
-        //var deferred = $q.defer();
-
-        //if (o.photos) {
-          //deferred.resolve(o.photos);
-        //}
-      //};
       o.hasPhotos = function() {
         return !!o.photos;
       };
