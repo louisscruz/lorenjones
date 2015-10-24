2fe0e63a77bbcabca62bc1fe1797e33a7c3de369
louisscruz <louisstephancruz@me.com>
2015-10-23 22:36:38 -0400

    Remove unneeded res.json

--- server/api/playlist/playlist.controller.js
+++ server/api/playlist/playlist.controller.js
@@ -66,7 +66,7 @@ exports.update = function(req, res) {
         }
         playlist.save(function(err) {
           if (err) { return handleError(res, err); }
-          res.status(200).json(playlist)
+          //res.status(200).json(playlist)
         })
         return res.status(200).json(playlist);
       });
