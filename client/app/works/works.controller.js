0119441b7a15c8e0aeb7579a036f84ee410409f7
louisscruz <louisstephancruz@me.com>
2015-10-24 21:59:28 -0400

    Add buttons for projects to works

--- client/app/works/works.controller.js
+++ client/app/works/works.controller.js
@@ -1,7 +1,23 @@
 'use strict';
 
 angular.module('lorenjonesApp')
-  .controller('WorksCtrl', function ($scope) {
+  .controller('WorksCtrl', function ($scope, works, $location) {
+    $scope.projects = [{
+      title: 'Dancing on the Brink of the World',
+      audio: 'https://soundcloud.com/lorenjones-2/14-the-city-by-the-sea',
+      link: '/dbw'
+    }, {
+      title: 'Woodward\'s Garden',
+      audio: '',
+      link: ''
+    }, {
+      title: 'Sweet Tommy',
+      audio: 'https://soundcloud.com/lorenjones-2/1-swingin-chains',
+      link: '/sweet-tommy'
+    }];
+    $scope.goTo = function(url) {
+      $location.path(url)
+    };
     $scope.info = '';
     $scope.toggle = function(id) {
       if (id === '' || id !== $scope.info) {
